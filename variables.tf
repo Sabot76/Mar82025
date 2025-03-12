9645b5b1250e70dfc9bd7e53867bbc0c217a8493
Aras Batu <batu@Aras-MacBook-Pro.local>
2025-03-12 19:48:27 +0100

    adedd variables and created resource for aim

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type = string
+}
+variable "github_usr" {
+  description = "GitHub Organization or Username"
+  type = string
+}
+variable "github_repo" {
+  description = "GitHub Repository Name"
+  type = string
+}
