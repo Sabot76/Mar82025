7358577ef2105c887ad17f20a18ed9f122e27f48
Aras Batu <batu@Aras-MacBook-Pro.local>
2025-03-17 16:59:50 +0100

    added private security group [skip ci]

--- resources.tf
+++ resources.tf
@@ -172,7 +172,7 @@ resource "aws_route_table_association" "private_b" {
   route_table_id = aws_route_table.private_rt.id
 }
 #Public Security Group
-resource "aws_security_group" "public_sq" {
+resource "aws_security_group" "public_sg" {
   name = "public/sg"
   vpc_id = aws_vpc.main_vpc.id
 
@@ -202,4 +202,25 @@ resource "aws_security_group" "public_sq" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# Private Security Group
+resource "aws_security_group" "private_sg" {
+  name = "private-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "SSH from Bastion only"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_groups = [aws_security_group.public_sg]
+  }
+  egress = {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
