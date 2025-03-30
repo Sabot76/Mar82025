b6b25f9e9873fccd041ee71b73944e8ad9c90cc0
Aras Batu <batu@Aras-MacBook-Pro.local>
2025-03-30 18:09:20 +0200

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,8 @@
-output "bastion_public_ip" {
+
+/* output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
 output "k3s_node_public_ip" {
   value = aws_instance.k3s_node.public_ip
 }
+ */
